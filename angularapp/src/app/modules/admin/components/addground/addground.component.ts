360ce3b07b5b2cf1750531098a5e47cf5e27fbf9
PriyankaDulam <78066574+PriyankaDulam@users.noreply.github.com>
2022-03-21 10:34:44 +0530

    Update addground.component.ts

--- angularapp/src/app/modules/admin/components/addground/addground.component.ts
+++ angularapp/src/app/modules/admin/components/addground/addground.component.ts
@@ -6,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addground.component.scss']
 })
 export class AddgroundComponent implements OnInit {
-  
+  GroundName=""
+  GroundPrice = ""
+  GroundLocation=""
+  GameType = ""
+  GroundDescription=""
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  
+  handleGround(){
+  console.log(this.GroundName);
+  console.log(this.GroundPrice);
+  console.log(this.GroundLocation);
+  console.log(this.GameType);
+  console.log(this.GroundDescription);
+}
 }
