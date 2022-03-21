8d4345d36ef7fb254742368bd487b93bbc057aad
PriyankaDulam <78066574+PriyankaDulam@users.noreply.github.com>
2022-03-21 10:28:58 +0530

    Update addadmin.component.ts

--- angularapp/src/app/modules/admin/components/addadmin/addadmin.component.ts
+++ angularapp/src/app/modules/admin/components/addadmin/addadmin.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addadmin.component.scss']
 })
 export class AddadminComponent implements OnInit {
+  firstName=""
+  lastName=""
+  emailId=""
+  PhoneNumber=""
+  password=""
+  repassword=""
 
   constructor() { }
 
   ngOnInit(): void {
   }
- 
+  adminSignup(){
+  console.log(this.firstName);
+  console.log(this.lastName);
+  console.log(this.emailId);
+  console.log(this.PhoneNumber);
+  console.log(this.password);
+  console.log(this.repassword);
+  }
 }
