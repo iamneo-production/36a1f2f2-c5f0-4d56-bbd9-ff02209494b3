6f0724f0c02118cb8cfb96d585fc61bb9f10a107
Ashwini J <53871761+Ashwinijustinraj@users.noreply.github.com>
2022-03-28 11:54:35 +0530

    Update grounds.ts

--- angularapp/src/app/shared/grounds.ts
+++ angularapp/src/app/shared/grounds.ts
@@ -9,25 +9,25 @@ export interface Ground {
     {
       id: 1,
       name: 'Extra Ground',
-      price: 799,
+      price: 1500,
       description: 'A large area with one of the best games'
     },
     {
       id: 2,
       name: 'OutDoor Ground',
-      price: 699,
+      price: 1500,
       description: 'A great place with one of the best ground'
     },
     {
       id: 3,
       name: 'Best Ground',
-      price: 599,
+      price: 1500,
       description: 'More Space More Games'
     },
     {
       id: 4,
       name: 'No.1 Ground',
-      price: 899,
+      price: 1500,
       description: 'Enjoy your time here with your family and friends'
     }
   ];
